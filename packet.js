8440f34be2b939284653272b0a90608c6b82dbf3
agarioBotDev <d.muenzer@mkl-gmbh.de>
2016-06-09 15:04:54 +0200

    Packet management

---
+++ packet.js
@@ -0,0 +1,202 @@
+//this file is for internal use with packets
+
+function Packet(e) {
+    this.data = e;
+
+    this.offset = 0;
+    
+}
+
+Packet.prototype = {
+    readUInt8: function(p) {
+        var offset = (typeof p) == 'number' ? p : this.offset;
+        var ret;
+        if(this.data.getUint8) {
+            ret = this.data.getUint8(offset);
+        }else{
+            ret = this.data.readUInt8(offset);
+        }
+        if(p === undefined) this.offset += 1;
+
+        return ret;
+    },
+    readInt16: function(p) {
+        var offset = (typeof p) == 'number' ? p : this.offset;
+        if (p == offset) {
+            
+            var byte1 = this.data[offset];
+            offset+=1;
+            var byte2 = this.data[offset];
+            offset+=1;
+            return byte1 << 8 | byte2;
+        }
+        this.offset+=1;
+        var byte1 = this.data[this.offset];
+        this.offset+=1;
+        var byte2 = this.data[this.offset];
+        
+        return byte1 << 8 | byte2;
+    },
+    read: function(p) {
+        var offset = (typeof p) == 'number' ? p : this.offset;
+        
+        return this.data[offset++];
+    },
+    readUInt16BE: function(p) {
+        var offset = (typeof p) == 'number' ? p : this.offset;
+        var ret;
+        if(this.data.getUint16) {
+            ret = this.data.getUint16(offset, false);
+        }else{
+            ret = this.data.readUInt16BE(offset);
+        }
+        if(p === undefined) this.offset += 2;
+
+        return ret;
+    },
+
+    readUInt16LE: function(p) {
+        var offset = (typeof p) == 'number' ? p : this.offset;
+        var ret;
+        if(this.data.getUint16) {
+            ret = this.data.getUint16(offset, true);
+        }else{
+            ret = this.data.readUInt16LE(offset);
+        }
+        if(p === undefined) this.offset += 2;
+
+        return ret;
+    },
+
+    readSInt16LE: function(p) {
+        var offset = (typeof p) == 'number' ? p : this.offset;
+        var ret;
+        if(this.data.getInt16) {
+            ret = this.data.getInt16(offset, true);
+        }else{
+            ret = this.data.readInt16LE(offset);
+        }
+        if(p === undefined) this.offset += 2;
+
+        return ret;
+    },
+
+    readUInt32LE: function(p) {
+        var offset = (typeof p) == 'number' ? p : this.offset;
+        var ret;
+        if(this.data.getUint32) {
+            ret = this.data.getUint32(offset, true);
+        }else{
+            ret = this.data.readUInt32LE(offset);
+        }
+        if(p === undefined) this.offset += 4;
+
+        return ret;
+    },
+
+    readUInt32BE: function(p) {
+        var offset = (typeof p) == 'number' ? p : this.offset;
+        var ret;
+        if(this.data.getUint32) {
+            ret = this.data.getUint32(offset, false);
+        }else{
+            ret = this.data.readUInt32BE(offset);
+        }
+        if(p === undefined) this.offset += 4;
+
+        return ret;
+    },
+
+    readSInt32LE: function(p) {
+        var offset = (typeof p) == 'number' ? p : this.offset;
+        var ret;
+        if(this.data.getInt32) {
+            ret = this.data.getInt32(offset, true);
+        }else{
+            ret = this.data.readInt32LE(offset);
+        }
+        if(p === undefined) this.offset += 4;
+
+        return ret;
+    },
+
+    readSInt32BE: function(p) {
+        var offset = (typeof p) == 'number' ? p : this.offset;
+        var ret;
+        if(this.data.getInt32) {
+            ret = this.data.getInt32(offset, false);
+        }else{
+            ret = this.data.readInt32BE(offset);
+        }
+        if(p === undefined) this.offset += 4;
+
+        return ret;
+    },
+
+    readFloat32LE: function(p) {
+        var offset = (typeof p) == 'number' ? p : this.offset;
+        var ret;
+        if(this.data.getFloat32) {
+            ret = this.data.getFloat32(offset, true);
+        }else{
+            ret = this.data.readFloatLE(offset);
+        }
+        if(p === undefined) this.offset += 4;
+
+        return ret;
+    },
+
+    readFloat32BE: function(p) {
+        var offset = (typeof p) == 'number' ? p : this.offset;
+        var ret;
+        if(this.data.getFloat32) {
+            ret = this.data.getFloat32(offset, false);
+        }else{
+            ret = this.data.readFloatBE(offset);
+        }
+        if(p === undefined) this.offset += 4;
+
+        return ret;
+    },
+
+    readFloat64LE: function(p) {
+        var offset = (typeof p) == 'number' ? p : this.offset;
+        var ret;
+        if(this.data.getFloat64) {
+            ret = this.data.getFloat64(offset, true);
+        }else{
+            ret = this.data.readDoubleLE(offset);
+        }
+        if(p === undefined) this.offset += 8;
+
+        return ret;
+    },
+
+    readFloat64BE: function(p) {
+        var offset = (typeof p) == 'number' ? p : this.offset;
+        var ret;
+        if(this.data.getFloat64) {
+            ret = this.data.getFloat64(offset, false);
+        }else{
+            ret = this.data.readDoubleBE(offset);
+        }
+        if(p === undefined) this.offset += 8;
+
+        return ret;
+    },
+
+    toString: function() {
+        var out = '';
+        for(var i=0;i<this.length;i++) {
+            if(out) out += ' ';
+            var char = this.readUInt8(i).toString(16);
+            if(char.length == 1) out += '0';
+            out += char;
+        }
+
+        return out;
+    }
+};
+
+module.exports = Packet;
+
